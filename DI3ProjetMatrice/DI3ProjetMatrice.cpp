9338909ed56bf11858621b91c7500d65d10515c3
SarahD27 <sarah.dns.sd@gmail.com>
2021-03-31 17:06:38 +0000

    made includes

--- DI3ProjetMatrice/DI3ProjetMatrice.cpp
+++ DI3ProjetMatrice/DI3ProjetMatrice.cpp
@@ -1,5 +1,11 @@
 // DI3ProjetMatrice.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
+#include "x64/headers/CException.h"
+#include "x64/headers/CFichier.h"
+#include "x64/headers/CMatrice.h"
+#include "x64/headers/CMatriceCaractere.h"
+#include "x64/headers/CMatriceDouble.h"
+#include "x64/headers/CMatriceEntier.h"
 
 #include <iostream>
 
