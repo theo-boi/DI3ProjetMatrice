dbf06d870e4c62f72ced74f8ffb7e4ba85ba5ce5
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-04-03 17:26:59 +0200

    Adaptation du compilateur .cmd

--- DI3ProjetMatrice/buildAndRunCpp.cmd
+++ DI3ProjetMatrice/buildAndRunCpp.cmd
@@ -1,7 +1,11 @@
 cd %userprofile%\source\repos\DI3ProjetMatrice\DI3ProjetMatrice\Debug
 del "DI3ProjetMatrice.exe"
+cl /W4 /EHsc ../source/DI3ProjetMatrice.cpp ../source/CException.cpp ../source/CFichier.cpp ../source/CMatrice.cpp ../source/CMatriceDouble.cpp ../source/CMatriceCaractere.cpp ../source/CMatriceEntier.cpp /link > DI3ProjetMatrice.tlog/log.txt
+cd DI3ProjetMatrice.tlog
 cls
 REM ***************** COMPILATION *****************
-cl /W4 /EHsc "../source/DI3ProjetMatrice.cpp" /link
+type log.txt
+REM *** Le contenu des logs de compilation est a : Debug/DI3ProjetMatrice.tlog/DI3ProjetMatrice.tlog ***
+cd ../
 REM **************** CONSOLE **********************
-"DI3ProjetMatrice.exe"
+DI3ProjetMatrice.exe
