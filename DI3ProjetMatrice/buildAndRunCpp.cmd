d8b39fa7bca3cc192b2f6593a651aa017a50b5fb
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-04-04 19:14:29 +0200

    Correction de buildAndRun.cmd

--- DI3ProjetMatrice/buildAndRunCpp.cmd
+++ DI3ProjetMatrice/buildAndRunCpp.cmd
@@ -1,5 +1,6 @@
 cd %userprofile%\source\repos\DI3ProjetMatrice\DI3ProjetMatrice\Debug
 del "DI3ProjetMatrice.exe"
+del "DI3ProjetMatrice.tlog/log.txt"
 cl /W4 /EHsc ../source/DI3ProjetMatrice.cpp ../source/C* ../source/Test.cpp /link > DI3ProjetMatrice.tlog/log.txt
 cd DI3ProjetMatrice.tlog
 cls
@@ -8,4 +9,4 @@ type log.txt
 REM *** Le contenu des logs de compilation est a : Debug/DI3ProjetMatrice.tlog/DI3ProjetMatrice.tlog ***
 cd ../
 REM ******************* CONSOLE *******************
-DI3ProjetMatrice.exe
+"DI3ProjetMatrice.exe"
