0e5a4c8e2f40bc22feaa1ed0a93647978152184f
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-04-04 17:01:43 +0200

    Correction de buildAndRun.cmd

--- DI3ProjetMatrice/buildAndRunCpp.cmd
+++ DI3ProjetMatrice/buildAndRunCpp.cmd
@@ -1,6 +1,6 @@
 cd %userprofile%\source\repos\DI3ProjetMatrice\DI3ProjetMatrice\Debug
 del "DI3ProjetMatrice.exe"
-cl /W4 /EHsc ../source/DI3ProjetMatrice.cpp ../source/CException.cpp ../source/CFichier.cpp ../source/CMatrice.cpp ../source/CMatriceDouble.cpp ../source/CMatriceCaractere.cpp ../source/CMatriceEntier.cpp /link > DI3ProjetMatrice.tlog/log.txt
+cl /W4 /EHsc ../source/DI3ProjetMatrice.cpp ../source/C* ../source/Test.cpp /link > DI3ProjetMatrice.tlog/log.txt
 cd DI3ProjetMatrice.tlog
 cls
 REM ***************** COMPILATION *****************
