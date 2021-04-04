d904779d931ddf9053cbc97e91bd84e138df200b
SarahD27 <>
2021-04-04 17:10:22 +0000

    Finished beginning of CFichier header

--- DI3ProjetMatrice/headers/CFichier.h
+++ DI3ProjetMatrice/headers/CFichier.h
@@ -12,4 +12,20 @@ class CFichier {
         char pcFICnom;
         FILE* pFICfichier;
         CMatriceDouble pMADmatrice;
+
+    //constructeur et destructeur
+    public :
+        CFichier();
+        ~CFichier();
+
+        //fonction principale
+        /*
+		 *	Methode sans arguments permettant de lire le contenu d'une matrice double dans un fichier formaté et de créer une matrice en mémoire à partie des données lues
+		 *
+		 *	Entree : rien
+		 *	Precondition : neant
+		 *	Sortie : MatriceDouble
+		 *	Postcondition : les données de la matrice correspondent à celles du fichier
+		 */
+        CMatriceDouble FICprincipale();
 };
