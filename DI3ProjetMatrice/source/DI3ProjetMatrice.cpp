31e23cfe81cb706ef69822794dd5fefd0afd6453
theo-boi <48034060+theo-boi@users.noreply.github.com>
2021-04-11 11:35:02 +0200

    Simplifications boucles for du main

--- DI3ProjetMatrice/source/DI3ProjetMatrice.cpp
+++ DI3ProjetMatrice/source/DI3ProjetMatrice.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]) {
 
 
 	//extraction des matrices depuis les fichiers entres en arguments de l'executable
-	for (unsigned int uiBoucleArgs = 0; uiBoucleArgs < uiNbFichiers; uiBoucleArgs++) {
+	for (unsigned int uiBoucleArgs = 0; uiBoucleArgs < uiNbFichiers; uiBoucleArgs++)
 		try {
 			CFichier fichier( argv[uiBoucleArgs+1] ); //instancie un CFichier temporaire et sa CMatrice
 			pMATDbdd[uiBoucleArgs] = fichier.FICgetCMatrice(); //sauvegarde la CMatrice dans pMATDbdd
@@ -30,13 +30,11 @@ int main(int argc, char *argv[]) {
 			cout << endl;
 			uiBoucleArgs--; uiNbFichiers--; //on considere un fichier de moins
 		}
-	}
 
 	//affichage de chaque matrice
 	cout << "Matrices de vos fichiers entres en arguments :\n\n";
-	for (unsigned int uiBoucleBdd = 0; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++) {
+	for (unsigned int uiBoucleBdd = 0; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++)
 		pMATDbdd[uiBoucleBdd].MATprint(1);
-	}
 	
 	//initialisation de c
 	double dConstante;
@@ -45,26 +43,23 @@ int main(int argc, char *argv[]) {
 
 	//affichage du resultat de la multiplication de chaque matrice par la valeur dConstante : c*M
 	cout << "Pour chaque matrice M, resultat de c*M :\n\n";
-	for (unsigned int uiBoucleBdd = 0; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++) {
+	for (unsigned int uiBoucleBdd = 0; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++)
 		try { (dConstante * pMATDbdd[uiBoucleBdd]).MATprint(1); }
 		catch (CException EXCmultConst) { EXCmultConst.EXCGestionaireException(); }
-	}
 
 	//affichage du resultat de la division de chaque matrice par la valeur dConstante : M/c
 	cout << "\nPour chaque matrice M, resultat de M/c :\n\n";
-	for (unsigned int uiBoucleBdd = 0; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++) {
+	for (unsigned int uiBoucleBdd = 0; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++)
 		try { (pMATDbdd[uiBoucleBdd] / dConstante).MATprint(1); }
 		catch (CException EXCdivConst) { EXCdivConst.EXCGestionaireException(); }
-	}
 
 
 	//affichage du resultat de la somme des matrices : M1+M2+M3+...
 	cout << "\nSomme des matrices";
 	CMatrice<double> MATDsomme(pMATDbdd[0]); //on a au moins une matrice
 	try {
-		for (unsigned int uiBoucleBdd = 1; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++) {
+		for (unsigned int uiBoucleBdd = 1; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++)
 			MATDsomme = MATDsomme + pMATDbdd[uiBoucleBdd];
-		}
 		cout << " = \n\n";
 		MATDsomme.MATprint(1);
 	}
@@ -77,12 +72,11 @@ int main(int argc, char *argv[]) {
 	cout << "\nSomme alternee des matrices";
 	CMatrice<double> MATDsommeAlternee(pMATDbdd[0]); //on a au moins une matrice
 	try {
-	for (unsigned int uiBoucleBdd = 1; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++) {
-		if ( !(uiBoucleBdd % 2) )
-			MATDsommeAlternee = MATDsommeAlternee + pMATDbdd[uiBoucleBdd];
-		else
+	for (unsigned int uiBoucleBdd = 1; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++)
+		if ( uiBoucleBdd % 2 )
 			MATDsommeAlternee = MATDsommeAlternee - pMATDbdd[uiBoucleBdd];
-	}
+		else
+			MATDsommeAlternee = MATDsommeAlternee + pMATDbdd[uiBoucleBdd];
 	cout << " = \n\n";
 	MATDsommeAlternee.MATprint(1);
 	}
@@ -96,11 +90,10 @@ int main(int argc, char *argv[]) {
 	CMatrice<double> MATDproduit(pMATDbdd[0]); //on a au moins une matrice
 	try {
 		//le sens de l'operation est important lors d'un produit matriciel
-		for (unsigned int uiBoucleBdd = 1; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++) {
+		for (unsigned int uiBoucleBdd = 1; uiBoucleBdd < uiNbFichiers; uiBoucleBdd++)
 			MATDproduit = MATDproduit * pMATDbdd[uiBoucleBdd]; //de gauche a droite
-		}
 		cout << " = \n\n";
-		MATDproduit.MATprint(1);
+		MATDproduit.MATprint(0);
 	}
 	catch (CException EXCsommeM) {
 		cout << endl;
